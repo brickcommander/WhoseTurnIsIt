a12de624980af477fc0d713cb4cc54b3a629e35d
brickcommander <pawan961746@gmail.com>
2024-09-20 01:58:29 +0530

    Added Work Model

---
+++ app/src/main/java/com/brickcommander/whoseturnisit/HomeActivity.kt
@@ -0,0 +1,37 @@
+package com.brickcommander.whoseturnisit
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.brickcommander.whoseturnisit.databinding.ActivityHomeBinding
+import com.brickcommander.whoseturnisit.logic.Calculate
+
+class HomeActivity : AppCompatActivity() {
+
+    // Declare binding variable
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var calculate: Calculate
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        // Inflate layout using ViewBinding
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //Initialize Calculate
+        calculate = Calculate()
+
+        binding.btnRefresh.setOnClickListener {
+            var res = calculate.updateToLatestDB()
+            binding.textView.text = "5"
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/brickcommander/whoseturnisit/model/Work.kt
@@ -0,0 +1,46 @@
+package com.brickcommander.whoseturnisit.model
+
+import java.time.LocalDate
+
+class Work() {
+    private var currentDate: LocalDate = LocalDate.now()
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    private var originalTurn: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    private var whoseTurnIsIt: String = "Default=Yashwant"
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    private var whoWillEat: List<String> = listOf("Anmol", "Yashwant", "Satyam", "Pawan")
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    private var isFoodCooked: Boolean = true
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    private var workDone: Boolean = false
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    override fun toString(): String {
+        var res = """Work[currentDate=${currentDate}, originalTurn=${originalTurn}, whoseTurnIsIt=${whoseTurnIsIt}, whoWillEat=${whoWillEat}, isFoodCooked=${isFoodCooked}, workDone=${workDone}]"""
+        return res
+    }
+}
